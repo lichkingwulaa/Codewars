8df30a3d890a4feb5da09d97d7092610eb677c9d
lichkingwulaa <lichkingwula@gmail.com>
2019-08-04 00:07:56 +0800

    continue

--- Sudoku Solver.py
+++ Sudoku Solver.py
@@ -1,3 +1,30 @@
+from random import randint
+
+
+def delete_zero_and_comp(array):
+	array = [x for x in array if x != 0]
+	if len(set(array)) != len(array):
+		return False
+
+def validSolution_isok(board):
+	"""检查未完成数组可行性"""
+	for i in range(9):
+		# row
+		delete_zero_and_comp(board[i])
+		# column
+		column = [board[j][i] for j in range(9)]
+		delete_zero_and_comp(column)
+
+	num = []
+	for x in range(0,3,6):  # 每列3个格子
+		for y in range(0,3,6):  # 每行3个格子
+			for i in range(x,x+3):  # 每个格子
+				for j in range(y,y+3):
+					num.append(board[i][j])
+			delete_zero_and_comp(num)
+
+	return True
+
 def validSolution(board):
 	for i in range(9):
 		# row
@@ -35,13 +62,15 @@ def sudoku(puzzle):
 				if len(board) == 0:
 					break
 				if len(board) != 0 and puzzle[i][j] == 0:
-					puzzle[i][j] = num(row[i],column[j]).pop(-1)
+					puzzle[i][j] = num(row[i],column[j]).pop(randint(0,len(board)-1))
 					row[i][j] = puzzle[i][j]
 					column[j][i] = puzzle[i][j]
+				if validSolution_isok(puzzle):
+					continue
 			else:
 				continue
 			break
-		if validSolution(puzzle):
+		if validSolution(puzzle) and len(board) == 0:
 			return puzzle
 	
 
--- something.py
+++ something.py
@@ -1,9 +1,28 @@
-for i in range(9):
-	for j in range(9):
-		if i*j == 6:
-			print(i*j)
-			break
-	else:
-		print('haha')
-		continue
-	break
+def delete_zero_and_comp(array):
+	array = [x for x in array if x != 0]
+	if len(set(array)) != len(array):
+		return False
+
+def validSolution_isok(board):
+	for i in range(9):
+		# row
+		delete_zero_and_comp(board[i])
+		# column
+		column = [board[j][i] for j in range(9)]
+		delete_zero_and_comp(column)
+
+	num = []
+	for x in range(0,3,6):  # 每列3个格子
+		for y in range(0,3,6):  # 每行3个格子
+			for i in range(x,x+3):  # 每个格子
+				for j in range(y,y+3):
+					num.append(board[i][j])
+			delete_zero_and_comp(num)
+
+	return True
+
+
+
+# 测试用
+from random import randint
+print(randint(0,1))
