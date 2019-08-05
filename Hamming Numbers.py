27a1af0a56a7b743a2f4ebe08664f221fae9fe13
lichkingwulaa <lichkingwula@gmail.com>
2019-08-05 17:07:12 +0800

    update

--- Hamming Numbers.py
+++ Hamming Numbers.py
@@ -17,3 +17,13 @@ def hamming(n):
 	return res[-1]
 
 print(hamming(1))
+
+
+
+# 大佬鼠的暴力解法
+# (确定最大汉明数，直接生成汉明数列表)
+
+h = sorted(2**i*3**j*5**k for i in range(35) for j in range(25) for k in range(15))
+
+def hamming(n):
+	return h[n-1]
